ff80afc6c7ec4cec8ac8e2f7263e05241e1b9855
Gayathridevi G <gopalakrishnangayathri589@gmail.com>
2026-03-09 23:25:36 +0530

    Add files via upload

---
+++ streamlitapp3/train_model.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+import pickle
+
+data = pd.read_csv("Iris.csv")
+
+X = data[["SepalLengthCm", "SepalWidthCm", "PetalLengthCm", "PetalWidthCm"]]
+y = data["Species"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = RandomForestClassifier()
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+
+print("Confusion Matrix:")
+print(confusion_matrix(y_test, y_pred))
+
+pickle.dump(model, open("model.pkl", "wb"))
